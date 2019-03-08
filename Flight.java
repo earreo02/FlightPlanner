49c26c998487a5a1e808bc3a97421fdec5da1da2
ong33414 <48310114+ong33414@users.noreply.github.com>
2019-03-07 17:53:00 -0800

    Add files via upload
    Implementing the flight class

--- Flight.java
+++ Flight.java
@@ -5,4 +5,27 @@
  */
 public class Flight {
 	//TODO: Implement me!
+	private String location, destination, time;
+	
+	public Flight(String location, String destination, String time) {
+		this.location = location;
+		this.destination = destination;
+		this.time  = time; 
+	}
+	
+	public String getLocation() {
+		return this.location;
+	}
+	
+	public String getDestination() {
+		return this.destination;
+	}
+	
+	public String getTime() {
+		return this.time;
+	}
+	
+	public String toString() {
+		return this.location + "->" + this.destination + ":" + this.time;
+	}
 }
